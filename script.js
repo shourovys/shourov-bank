77b0800004988a3ee1bce8aa1102f43fdddd7146
shourov saha <shourovys@gmail.com>
2020-03-03 10:11:19 +0600

    complit 1st page

---
+++ script.js
@@ -0,0 +1,9 @@
+
+    <script>
+    document.getElementById("change").addEventListener("click", function(){
+      document.getElementById("login-area").style.display = "none";
+      
+    })
+
+
+    </script>
